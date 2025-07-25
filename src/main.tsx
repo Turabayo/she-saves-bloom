953a1101c281ae325bba0246e570384709f1e54a
turabayo <i.turabayo@alustudent.com>
2025-07-25 14:19:43 +0200

    Fix: Wrap App with HashRouter for mobile build

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import App from './App.tsx'
 import './index.css'
 import { Toaster } from '@/components/ui/toaster'
 import { LanguageProvider } from '@/contexts/LanguageContext'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById("root")!).render(
   <LanguageProvider>
-    <App />
-    <Toaster />
+    <HashRouter>
+      <App />
+      <Toaster />
+    </HashRouter>
   </LanguageProvider>
 );
